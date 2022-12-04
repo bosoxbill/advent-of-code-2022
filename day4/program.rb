ef203004ae0292e65745b6bce357b5462ed72e26
Bill Desmarais <bill@outcomes4me.com>
2022-12-04 11:45:04 -0500

    day 4 part 1

---
+++ day4/program.rb
@@ -0,0 +1,23 @@
+INPUT = ENV['INPUT'] ? ENV['INPUT'] : 'input.txt'
+DEBUG = ENV['DEBUG'] ? true : false
+
+def debug_print(stringish)
+  pp stringish if DEBUG
+end
+
+def overlaps(r1, r2)
+  r2.include?(r1.first) && r2.include?(r1.last) ||
+    r1.include?(r2.first) && r1.include?(r2.last)
+end
+
+@count = 0
+
+file_lines = File.readlines(INPUT)
+file_lines.each do |line|
+  first, second = line.chomp.split(',').map{|rs| rs.split('-').map(&:to_i)}
+  first_range = first[0]..first[1]
+  second_range = second[0]..second[1]
+  @count += 1 if overlaps(first_range, second_range)
+end
+
+puts @count
